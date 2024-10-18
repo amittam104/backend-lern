50c004d1ee94361b0562d8fd1ffabf1e22a7d108
Amit Tambulkar <142736996+amittam104@users.noreply.github.com>
2024-10-18 09:14:53 +0000

    chore: add user and video db model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,50 @@
-import mongoose, { Schema } from "mongooses";
+import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({}, {});
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
