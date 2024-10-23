3c384120e1b1419c2cb696386e0c4bc91c7d6d12
Amit Tambulkar <142736996+amittam104@users.noreply.github.com>
2024-10-23 08:26:22 +0000

    chore: add register user controller and route

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,7 @@
+import { asyncHandler } from "../utils/asyncHandler";
+
+export const registerUser = asyncHandler(async function (req, res) {
+  return res.status(200).json({
+    message: ok,
+  });
+});
