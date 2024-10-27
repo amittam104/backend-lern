21ed1199ee68ec375fb44c0eff78ee03fe2ea335
Amit Tambulkar <142736996+amittam104@users.noreply.github.com>
2024-10-27 04:20:06 +0000

    chore: user channel aggregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -313,3 +313,69 @@ export const updateUserCoverImage = asyncHandler(async function (req, res) {
     .status(200)
     .json(new ApiResponse(200, user, "Cover image updated successfully"));
 });
+
+export const getUserChannelProfile = asyncHandler(async function (req, res) {
+  const { username } = req.params;
+
+  if (!username?.trim()) throw new ApiError(400, "Username is missing");
+
+  const channel = await User.aggregate([
+    {
+      $match: { username: username?.toLowerCase() },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelSubscribedTo: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelSubscribedTo: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) throw new ApiError(400, "Channel could not be found");
+
+  console.log(channel);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User channel fetched successfully")
+    );
+});
